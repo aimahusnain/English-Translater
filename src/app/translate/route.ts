fd56ac7a7218a8cf66c1330130232a0c98743549
aimahusnain <aimahusnain@gmail.com>
2024-01-01 17:58:34 +0500

    first commit

---
+++ src/app/translate/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { exec } from "child_process";
+
+export async function POST(request: Request) {
+	const { text } = await request.json();
+
+	const translatedTextPromise = new Promise((resolve, reject) => {
+		exec(
+			`python translate.py "${text}"`,
+			(error, stdout, stderr) => {
+				if (error) {
+					console.error(`exec error: ${error}`);
+					reject(error);
+				}
+				resolve(stdout);
+			}
+		);
+	});
+
+	const translatedText = await translatedTextPromise;
+	return NextResponse.json({ translatedText });
+}
